7c82491505a853226ce56c3c3505d64255505cf6
MengxiKate <99297858+Mengxicici@users.noreply.github.com>
2024-04-21 15:45:45 -0500

    instruction

--- app.py
+++ app.py
@@ -419,7 +419,7 @@ if 'df' in st.session_state:
 
     # Assuming 'adata' and 'ProteomicNormalizer' are defined correctly
     N_MD="""
-      - Normalization is a mandatory step before performing any downstream analysis. Further analysis will only show after Normalization. Please select the normalization method and click on "Apply Normalization" to proceed.
+      - Normalization is a mandatory step before performing any downstream analysis. Further analysis will only show after Normalization. Please select the normalization method and click on "Apply Normalization" to proceed.:face_with_raised_eyebrow:
       """
 
     st.warning('Please Note', icon="⚠️")
